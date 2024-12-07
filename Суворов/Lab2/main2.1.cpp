6d92e880b64ec341b6323d6efa54f81137c966bc
AlabamaWebp <dimyrp@yandex.ru>
2024-12-07 18:11:15 +0300

    1

---
+++ Суворов/Lab2/main2.1.cpp
@@ -0,0 +1,81 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+bool validate(string row)
+{
+    if (row.size() < 2 || row.size() > 16)
+        return false;
+    for (int i = 0; i < row.size(); i++)
+    {
+        if (row[i] != '0' && row[i] != '1')
+            return false;
+    }
+    return true;
+}
+void transformate(string &row, string &obr, string &dop)
+{
+    for (int i = 1; i < obr.size(); i++)
+    {
+        if (obr[i] == '0')
+            obr[i] = '1';
+        else
+            obr[i] = '0';
+    }
+    dop = obr;
+    bool noall = true;
+    for (int i = obr.size(); i > 1; i--)
+    {
+        if (obr[i] == '0')
+        {
+            dop[i] = '1';
+            noall = false;
+            break;
+        }
+        else
+            dop[i] = '0';
+    }
+    if (noall)
+        for (int i = 0; i < obr.size(); i++)
+            dop[i] = '0';
+}
+void runTask(string filename1, string filename2)
+{
+    ifstream file1(filename1);
+    ofstream file2(filename2, ios::trunc);
+    if (!file1.is_open() || !file2.is_open())
+    {
+        cerr << "ERROR FILE OPEN!" << endl;
+        return;
+    }
+    string row;
+    string obr;
+    string dop;
+    while (getline(file1, row))
+    {
+        if (!validate(row))
+        {
+            file2 << row << ",ERROR" << endl;
+            continue;
+        }
+        obr = row;
+        if (row[0] == '1')
+        {
+            transformate(row, obr, dop);
+        }
+        else
+            dop = row;
+        file2 << row << "," << obr << "," << dop << endl;
+    }
+    file1.close();
+    file2.close();
+}
+
+int main()
+{
+    string input_file = "si25_eliseev_task1.csv";
+    string output_file = "result2_eliseev_3.csv";
+    runTask(input_file, output_file);
+    return 0;
+}
