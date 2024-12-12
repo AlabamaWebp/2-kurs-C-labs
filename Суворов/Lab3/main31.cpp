74bd303d324753fc189b542d87a6f6759cd4d633
AlabamaWebp <dimyrp@yandex.ru>
2024-12-12 19:51:49 +0300

    1

---
+++ Суворов/Lab3/main31.cpp
@@ -0,0 +1,41 @@
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+float transformate(string &row)
+{
+    float tmp;
+    float min;
+    int last_index = 0;
+    last_index = row.find(",");
+    min = stof(row.substr(0, last_index));
+    for (int i = 0; i < row.size(); i++)
+    {
+        last_index = row.find(",", last_index);
+    }
+}
+void runTask(string filename1, string filename2)
+{
+    ifstream file1(filename1);
+    if (!file1.is_open() || !file2.is_open())
+    {
+        cerr << "ERROR FILE OPEN!" << endl;
+        return;
+    }
+    string row;
+    getline(file1, row)
+        file1.close();
+
+    ofstream file2(filename2, ios::trunc);
+    file2.close();
+}
+
+int main()
+{
+    string input_file = "task31.csv";
+    string output_file = "result31.csv";
+    runTask(input_file, output_file);
+    return 0;
+}
