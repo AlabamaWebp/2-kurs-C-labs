e256a0ee7f9f20bb3a1da76f5e4bf4351e2e5cde
AlabamaWebp <dimyrp@yandex.ru>
2024-12-17 19:25:12 +0300

    Суворов 3.1 работает

--- Суворов/Lab3/main31.cpp
+++ Суворов/Lab3/main31.cpp
@@ -4,32 +4,41 @@
 #include <string>
 
 using namespace std;
-float transformate(string &row)
+void transformate(string &row, float &min)
 {
     float tmp;
-    float min;
     int last_index = 0;
+    int tmp_i;
     last_index = row.find(",");
     min = stof(row.substr(0, last_index));
-    for (int i = 0; i < row.size(); i++)
+    while (last_index != -1)
     {
-        last_index = row.find(",", last_index);
+        tmp_i = row.find(",", last_index + 1);
+        if (tmp_i == -1)
+            tmp = stof(row.substr(last_index + 1));
+        else
+            tmp = stof(row.substr(last_index + 1, tmp_i - last_index - 1));
+        if (tmp < min)
+            min = tmp;
+        last_index = tmp_i;
     }
 }
 void runTask(string filename1, string filename2)
 {
     ifstream file1(filename1);
-    if (!file1.is_open() || !file2.is_open())
+    if (!file1.is_open())
     {
         cerr << "ERROR FILE OPEN!" << endl;
         return;
     }
     string row;
     getline(file1, row);
-    transformate(row);
+    float min;
+    transformate(row, min);
     file1.close();
 
     ofstream file2(filename2, ios::trunc);
+    file2 << min;
     file2.close();
 }
 
