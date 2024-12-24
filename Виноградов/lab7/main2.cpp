8a09a3dbc8290375c367bc5cb393207b8e0c24df
AlabamaWebp <dimyrp@yandex.ru>
2024-12-24 14:21:02 +0300

    7.2

--- Виноградов/lab7/main2.cpp
+++ Виноградов/lab7/main2.cpp
@@ -4,33 +4,46 @@
 #include <numeric>
 
 using namespace std;
-void output(vector<double> arr)
-{
-    for (double &elem : arr)
-    {
-        cout << elem << " ";
-    }
-    cout << endl;
-}
-int main()
+
+class task2
 {
     const int N = 10;
     vector<double> arr(N);
-    generate(arr.begin(), arr.end(), []()
-             { return (rand() % 20 - 10) / 1.0; });
 
-    auto pos_elem = find_if(arr.begin(), arr.end(), [](double x){ return x > 0; });
-    double sum = accumulate(pos_elem + 1, arr.end(), 0.0);
-    cout << "Сумма элементов после первого положительного: " << sum << endl;
-
-    output(arr);
-
-    double a = -2.0, b = 2.0;
-    partition(arr.begin(), arr.end(), [a, b](double x)
-              { return x >= a && x <= b; });
-
+public:
+    task2()
+    {
+        generate(arr.begin(), arr.end(), []()
+                 { return (rand() % 20 - 10) / 1.0; });
+        output();
+    }
+    void output()
+    {
+        for (double &elem : arr)
+        {
+            cout << elem << " ";
+        }
+        cout << endl;
+    }
+    double sum()
+    {
+        auto pos_elem = find_if(arr.begin(), arr.end(), [](double x)
+                                { return x > 0; });
+        return accumulate(pos_elem + 1, arr.end(), 0.0);
+    }
+    void rebase()
+    {
+        double a = -2.0, b = 2.0;
+        partition(arr.begin(), arr.end(), [a, b](double x)
+                  { return x >= a && x <= b; });
+    }
+} int main()
+{
+    task2 t;
+    cout << "Сумма элементов после первого положительного: " << t.sum << endl;
     cout << "Преобразованный массив: ";
     output(arr);
-
     return 0;
 }
+// сумму элементов массива, расположенных после первого положительного элемента. Преобразовать массив таким образом,
+// чтобы сначала располагались все элементы, целая часть которых лежит в интервале [а,Ь], а потом - все остальные.
